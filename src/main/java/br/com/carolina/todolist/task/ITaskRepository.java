53c72c23b84ac2b0083578a1edae27f52369409f
Carolina Carvalho <carolina.dossantos0806@gmail.com>
2023-10-13 15:07:06 -0300

    aula 4 e aula 5

--- src/main/java/br/com/carolina/todolist/task/ITaskRepository.java
+++ src/main/java/br/com/carolina/todolist/task/ITaskRepository.java
@@ -1,9 +1,13 @@
 package br.com.carolina.todolist.task;
 
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ITaskRepository extends JpaRepository<TaskModel, UUID> {
-    
+
+    List<TaskModel> findByIdUser(UUID user);
+
+    TaskModel findByIdAndIdUser(UUID id, UUID user);
 }
--- src/main/java/br/com/carolina/todolist/task/TaskController.java
+++ src/main/java/br/com/carolina/todolist/task/TaskController.java
@@ -1,10 +1,22 @@
 package br.com.carolina.todolist.task;
 
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.carolina.todolist.utils.Utils;
+import jakarta.servlet.http.HttpServletRequest;
 
 @RestController
 @RequestMapping("/tasks")
@@ -13,10 +25,45 @@ public class TaskController {
     @Autowired
     private ITaskRepository taskRepository;
     
-    @PostMapping("/create")
-    public TaskModel create(@RequestBody TaskModel taskModel) {
+    @PostMapping("/")
+    public ResponseEntity create(@RequestBody TaskModel taskModel, HttpServletRequest request) {
+        taskModel.setIdUser((UUID) request.getAttribute("idUser"));
+
+        var currentDate = LocalDateTime.now();
+        if(currentDate.isAfter(taskModel.getStartAt()) || currentDate.isAfter(taskModel.getEndAt())) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Data errada");
+        }
+
+        if(taskModel.getStartAt().isAfter(taskModel.getEndAt())) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("A data inicio deve ser menor do que a data de término");
+        }
+
         var task = this.taskRepository.save(taskModel);
-        return task;
+        return new ResponseEntity<TaskModel>(task, HttpStatus.OK);
     }
 
+    @GetMapping("/")
+    public List<TaskModel> list(HttpServletRequest request) {
+        var idUser = request.getAttribute("idUser");
+        var listTasks = this.taskRepository.findByIdUser((UUID) idUser);
+
+        return listTasks;
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity update(@RequestBody TaskModel taskModel, HttpServletRequest request, @PathVariable UUID id) {
+        var idUser = request.getAttribute("idUser");
+
+        var task = this.taskRepository.findByIdAndIdUser((UUID) id, (UUID) idUser);
+
+        if (task == null) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Tarefa não encontrada");
+        }
+
+        Utils.copyNonNullProperties(taskModel, task);
+
+        this.taskRepository.save(task);
+
+        return ResponseEntity.ok().body(task);
+    }
 }
